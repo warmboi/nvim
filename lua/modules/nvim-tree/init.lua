902349cf25b5ff67c8cafe99d35c169f1dcf6293
warmboi <warm_boi@naver.com>
2022-09-08 21:29:19 +0900

    fix: nvim-tree 리스팅 보이지 않는 문제 수정

--- lua/modules/nvim-tree/init.lua
+++ lua/modules/nvim-tree/init.lua
@@ -1,5 +1,18 @@
 require('nvim-tree').setup {
   open_on_setup = true,
-  open_on_setup_file = true
+  open_on_setup_file = true,
+  git = {
+	enable = true,
+	ignore = false,
+	show_on_dirs = true,
+  },
+  filters = {
+	dotfiles = false,
+	custom = {
+	  "node_modules/*",
+	  "dist/*",
+	  "out/*",
+	}
+  }
 }
 
