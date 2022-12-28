5698a693182af6efd39c7707574ef36ff7087a19
jacob <jacob@111percent.net>
2022-12-28 11:27:29 +0900

    fix: 수정

--- init.sh
+++ init.sh
@@ -32,10 +32,12 @@ let g:ale_fixers ={'javascript': ['prettier', 'eslint'], 'typescript': ['prettie
 let g:ale_linters_explicit = 1
 let g:ale_fix_on_save = 1
 let g:fugitive_dynamic_colors = 0
+let mapleader=" "
 
 vnoremap d "_d
 nnoremap dd "_dd
 nnoremap D "_D
+nnoremap <SPACE> <Nop>
 
 nnoremap <silent> K :call ShowDocumentation()<CR>
 function! CheckBackspace() abort
